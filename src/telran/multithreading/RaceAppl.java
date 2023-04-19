c0cf942b4bd72f34b294cddef442d7b736402333
stanislav-revva <nivadrive1@gmail.com>
2023-04-19 12:59:19 +0300

    commit 19-04-2023

--- src/telran/multithreading/RaceAppl.java
+++ src/telran/multithreading/RaceAppl.java
@@ -1,53 +1,79 @@
 package telran.multithreading;
 
-
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.stream.IntStream;
 
 import telran.view.*;
 
 public class RaceAppl {
-	
-	public static void main (String [] args) {
+
+	private static final int MAX_THREADS = 20;
+	private static final int MIN_DISTANCE = 10;
+	private static final int MAX_DISTANCE = 1000;
+	private static final int MIN_SLEEP = 2;
+	private static final int MAX_SLEEP = 5;
+	public static void main(String[] args) {
 		InputOutput io = new StandardInputOutput();
-		Menu menu = new Menu("Racing", Item.of("Start Racing", RaceAppl::startRace), Item.of("Exit", n -> n.writeLine("Good buy"), true));
+		Item[] items = getItems();
+		Menu menu = new Menu("Race Game", items);
 		menu.perform(io);
+
 	}
+
+	private static Item[] getItems() {
+		Item[] res = {
+				Item.of("Start new game", RaceAppl::startGame),
+				Item.exit()
+		};
+		return res;
+	}
+	static void startGame(InputOutput io) {
+		int nThreads = io.readInt("Enter number of the runners","Wrong number of the runners", 2, MAX_THREADS);
+		int distance = io.readInt("Enter distance", "Wrong Distance",MIN_DISTANCE, MAX_DISTANCE);
+		Race race = new Race(distance, MIN_SLEEP, MAX_SLEEP, new ArrayList<Runner>(), Instant.now());
+		Runner[] runners = new Runner[nThreads];
+		startRunners(runners, race);
+		joinRunners(runners);
+		displayResultsTable(race);
+	}
+
 	
-	static void startRace(InputOutput io) {
-		int quantity = Integer.parseInt(io.readString("Enter number of racers (2-10)"));
-		if (quantity < 2 || quantity > 10) {
-			io.writeLine("Quantity of racers must be between 2-10");
-			quantity = Integer.parseInt(io.readString("Enter number of racers (2-10)"));
-		}
-		int distance = Integer.parseInt(io.readString("Enter distance (100-1000)"));
-		if (distance < 100 || distance > 1000) {
-			io.writeLine("Distance of race must be between 100-1000");
-			distance = Integer.parseInt(io.readString("Enter distance (100-1000)"));
-		}
-		List<Racer> racerList = new ArrayList<Racer>();
-		long res;
-		Instant start = Instant.now();
-		for (int i = 1; i <= quantity; i++) {
-			Racer racer = new Racer(i, distance);
-			racer.setStart(start);
-			racerList.add(racer);
-			racer.start();
-					}
-		for (Racer r : racerList) {
+
+	private static void joinRunners(Runner[] runners) {
+		IntStream.range(0, runners.length).forEach(i -> {
 			try {
-				r.join();
+				runners[i].join();
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				throw new IllegalStateException();
 			}
-		}
-			
-		io.writeLine(String.format("Winner is - racer #%d", Racer.isFinish.get()));
+		});
+		
+	}
+
+	private static void startRunners(Runner[] runners, Race race) {
+		IntStream.range(0, runners.length).forEach(i -> {
+			runners[i] = new Runner(race, i + 1);
+			runners[i].start();
+		});
+		
+	}
+	private static void displayResultsTable(Race race) {
+		System.out.println("place\tracer number\ttime");
+		List<Runner> resultsTable = race.getResultsTable();
+		IntStream.range(0, resultsTable.size()).mapToObj(i ->  toPrintedString(i, race))
+		.forEach(System.out::println);
+		
+		
+		
+		
+	}
+	private static String toPrintedString(int index, Race race) {
+		Runner runner = race.getResultsTable().get(index);
+		return String.format("%3d\t%7d\t\t%d", index + 1, runner.getRunnerId(),
+				ChronoUnit.MILLIS.between(race.getStartTime(), runner.getFinsishTime()));
 	}
 
 }
